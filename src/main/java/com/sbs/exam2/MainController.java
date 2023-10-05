38d8680c758736ad8c3221c5d2248d0bf82beab6
hyeonjae1023 <hyeonjae1023111@gmail.com>
2023-10-06 01:00:28 +0900

    2장 완료

--- src/main/java/com/sbs/exam2/MainController.java
+++ src/main/java/com/sbs/exam2/MainController.java
@@ -12,4 +12,9 @@ public class MainController {
     public String index() {
         return "exam2";
     }
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/question/list";
+    }
 }
---
+++ src/main/java/com/sbs/exam2/question/QuestionService.java
@@ -0,0 +1,37 @@
+package com.sbs.exam2.question;
+
+import com.sbs.exam2.DataNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = questionRepository.findById(id);
+        if(question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+
+    public void create(String subject, String content) {
+        Question question = new Question();
+        question.setSubject(subject);
+        question.setContent(content);
+        question.setCreateDate(LocalDateTime.now());
+        this.questionRepository.save(question);
+    }
+}
