9c5f502d5b89a29a6a455aa785e4666cf9165293
hyeonjae1023 <hyeonjae1023111@gmail.com>
2023-10-25 13:47:35 +0900

    소셜 로그인 다시

--- src/main/java/com/sbs/exam2/user/SiteUser.java
+++ src/main/java/com/sbs/exam2/user/SiteUser.java
@@ -3,6 +3,11 @@ package com.sbs.exam2.user;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,4 +24,28 @@ public class SiteUser {
 
     @Column(unique = true)
     private String email;
+
+    private String nickName;
+
+    private String profileImgUrl;
+
+    public List<? extends GrantedAuthority> getGrantedAuthorities() {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+
+        // 모든 멤버는 member 권한을 가진다.
+        grantedAuthorities.add(new SimpleGrantedAuthority("user"));
+
+        // username이 admin인 회원은 추가로 admin 권한도 가진다.
+        if(isAdmin()) {
+            grantedAuthorities.add(new SimpleGrantedAuthority("admin"));
+        }
+
+        return grantedAuthorities;
+
+
+    }
+
+    private boolean isAdmin() {
+        return "admin".equals(username);
+    }
 }
