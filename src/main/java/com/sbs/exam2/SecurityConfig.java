810fa07f82c6db130861d00d63e8d8e3491a6175
hyeonjae1023 <hyeonjae1023111@gmail.com>
2023-10-25 12:53:11 +0900

    소셜로그인 완료. db저장 작업 필요

--- src/main/java/com/sbs/exam2/SecurityConfig.java
+++ src/main/java/com/sbs/exam2/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.sbs.exam2;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -14,13 +16,24 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
+
+    private final OAuth2UserService oAuth2UserService;
+
+    public SecurityConfig(OAuth2UserService oAuth2UserService) {
+        this.oAuth2UserService = oAuth2UserService;
+    }
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -30,10 +43,10 @@ public class SecurityConfig {
                         .loginPage("/user/login")
                         .loginProcessingUrl("/user/login")
                         .defaultSuccessUrl("/"))
-                .oauth2Login(
-                        oauth2Login -> oauth2Login
-                                .loginPage("/user/login")
-                                .defaultSuccessUrl("/")
+                .oauth2Login(oauth2Configurer -> oauth2Configurer
+                        .loginPage("/login")
+                        .successHandler(successHandler())
+                        .userInfoEndpoint(u -> u.userService(oAuth2UserService))
                 )
                 .logout((logout) -> logout
                         .logoutRequestMatcher(new AntPathRequestMatcher("/user/logout"))
@@ -42,6 +55,25 @@ public class SecurityConfig {
         ;
         return http.build();
     }
+
+    private AuthenticationSuccessHandler successHandler() {
+        return ((request, response, authentication) -> {
+            DefaultOAuth2User defaultOAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
+
+            String id = defaultOAuth2User.getAttributes().get("id").toString();
+            String body = """
+                    {"id":"%s"}
+                    """.formatted(id);
+
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+            PrintWriter writer = response.getWriter();
+            writer.println(body);
+            writer.flush();
+        });
+    }
+
     @Bean
     AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
         return authenticationConfiguration.getAuthenticationManager();
--- src/main/java/com/sbs/exam2/user/UserController.java
+++ src/main/java/com/sbs/exam2/user/UserController.java
@@ -1,39 +1,21 @@
 package com.sbs.exam2.user;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sbs.exam2.answer.Answer;
 import com.sbs.exam2.answer.AnswerService;
 import com.sbs.exam2.comment.Comment;
 import com.sbs.exam2.comment.CommentService;
-import com.sbs.exam2.model.KakaoProfile;
-import com.sbs.exam2.model.OAuthToken;
 import com.sbs.exam2.question.Question;
 import com.sbs.exam2.question.QuestionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
-
 import java.security.Principal;
-import java.util.UUID;
-
-import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
 
 @RequestMapping("/user")
 @RequiredArgsConstructor
@@ -44,7 +26,7 @@ public class UserController {
     private final QuestionService questionService;
     private final AnswerService answerService;
     private final CommentService commentService;
-    private AuthenticationManager authenticationManager;
+
 
     @GetMapping("/signup")
     public String signup(UserCreateForm userCreateForm){
@@ -80,78 +62,6 @@ public class UserController {
         return "login_form";
     }
 
-    @GetMapping("/auth/kakao/callback")
-    public String kakaoCallback(String code) throws JsonProcessingException { //Data를 리턴해주는 컨트롤러
-        //POST방식으로 key=value 데이터를 요청(카카오 쪽으로)
-        RestTemplate rt = new RestTemplate();
-
-        //HttpHeader 오브젝트 생성
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
-
-        //HttpBody 오브젝트 생성
-        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        params.add("grant_type","authorization_code");
-        params.add("client_id","eaf798efc47254e12eba3a9a52b72302");
-        params.add("redirect_uri","http://localhost:8090/user/auth/kakao/callback");
-        params.add("code",code);
-
-        //HttpHeader 와 HttpBody를 하나의 오브젝트에 담기
-        HttpEntity<MultiValueMap<String, String>> kakaoTokenRequest =
-        new HttpEntity<>(params, headers);
-
-        //Http 요청하기 - Post방식으로 - 그리고 response 변수의 응답 받음.
-        ResponseEntity<String> response = rt.exchange(
-                "https://kauth.kakao.com/oauth/token",
-                HttpMethod.POST,
-                kakaoTokenRequest,
-                String.class
-        );
-
-        //Gson, Json Simple, ObjectMapper
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        OAuthToken oAuthToken = objectMapper.readValue(response.getBody(), OAuthToken.class);
-
-        System.out.println("카카오 엑세스 토큰: " + oAuthToken.getAccess_token());
-
-        RestTemplate rt2 = new RestTemplate();
-
-        //HttpHeader 오브젝트 생성
-        HttpHeaders headers2 = new HttpHeaders();
-        headers2.add("Authorization","Bearer "+oAuthToken.getAccess_token());
-        headers2.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
-
-        //HttpHeader 와 HttpBody를 하나의 오브젝트에 담기
-        HttpEntity<MultiValueMap<String, String>> kakaoProfileRequest =
-                new HttpEntity<>(headers2);
-
-        //Http 요청하기 - Post방식으로 - 그리고 response 변수의 응답 받음.
-        ResponseEntity<String> response2 = rt2.exchange(
-                "https://kapi.kakao.com/v2/user/me",
-                HttpMethod.POST,
-                kakaoProfileRequest,
-                String.class
-        );
-
-        ObjectMapper objectMapper2 = new ObjectMapper();
-
-        KakaoProfile kakaoProfile = objectMapper2.readValue(response2.getBody(), KakaoProfile.class);
-
-        System.out.println("카카오 아이디(번호): " + kakaoProfile.getId());
-        System.out.println("카카오 이메일: " + kakaoProfile.getId()+"@gmail.com");
-        System.out.println("카카오 username: " + kakaoProfile.getId()+"__");
-        UUID garbagePw = UUID.randomUUID();
-        System.out.println("카카오 비번: "+garbagePw);
-
-        SiteUser user = new SiteUser();
-
-        userService.create(kakaoProfile.getId()+"__", kakaoProfile.getId()+"@gmail.com", garbagePw.toString());
-
-        //로그인 처리
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUsername(),user.getPassword()));
-        return "redirect:/";
-    }
     @GetMapping("/changePassword")
     public String changePasswordForm(UserPasswordForm userPasswordForm) {
         return "password_form"; // 이는 changePassword.html 페이지를 나타냅니다.
